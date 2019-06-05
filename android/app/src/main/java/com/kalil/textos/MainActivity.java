087b47a1fe252187db24205617e34533d9c1a0df
KalilDev <kalilgamer0@gmail.com>
2019-06-04 21:56:19 -0300

    Properly disable status bar
    * TODO: account for notches

--- android/app/src/main/java/com/kalil/textos/MainActivity.java
+++ android/app/src/main/java/com/kalil/textos/MainActivity.java
@@ -1,6 +1,9 @@
 package com.kalil.textos;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
@@ -10,8 +13,22 @@ public class MainActivity extends FlutterActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        int uiOptions;
+        if (Build.VERSION.SDK_INT > 19) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                uiOptions=View.SYSTEM_UI_FLAG_IMMERSIVE;
+            } else {
+                uiOptions=View.SYSTEM_UI_FLAG_IMMERSIVE;
+            }
+        } else {
+            uiOptions=View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+        }
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
         mNotificationUtils = new NotificationUtils(this);
         super.onCreate(savedInstanceState);
         GeneratedPluginRegistrant.registerWith(this);
     }
+
 }
